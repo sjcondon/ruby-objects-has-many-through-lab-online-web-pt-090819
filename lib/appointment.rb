eea9e7340b8ce25c144be79d6217e43db79eca9b
sarah <s.j.condon01@gmail.com>
2019-10-15 01:33:16 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,9 +1,16 @@
 class Appointment
   attr_accessor :name
 
-  def initialize(date, doctor)
-    @date = date
-    @doctor = doctor
-    doctor.add_appointment(self)
+  def initialize(name)
+    @name = name
+    end
+  
+  def patient
+    patient.all.select{|patient| patient.appointment == self}
   end
+  
+  def doctor
+    patient.map{|patient| patient.doctor}
+  end
+  
 end
